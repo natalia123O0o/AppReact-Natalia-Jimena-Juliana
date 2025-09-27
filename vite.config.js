ffd996678360c48535eb4a6268d26edd9d10d1bc
Natalia <yinaasdfd@gmail.com>
2025-09-27 08:57:59 -0500

    cambios nuevos

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/AppReact-Natalia-Jimena-Juliana/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  
 })
